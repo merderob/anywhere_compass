7a09c3808d0b9db8b919c01a7ede482b71a58b86
merderob <merderob@yahoo.yom>
2024-01-21 01:38:47 +0100

    Added display for GPS calibration.

--- src/display.cpp
+++ src/display.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2023 Robert Takacs
+// Copyright © 2024 Robert Takacs
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
 // files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -12,47 +12,50 @@
 // COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include <Wire.h>
-#include <HMC5883L.h>
+#include "display.h"
+#include "params.h"
+#include <Arduino.h>
 
-HMC5883L compass;
-
-void setup() 
+Display::Display(params::DisplayParams p):
+    green_led_location_(p.green_led_location), 
+    red_led_location_(p.red_led_location)
 {
-    Serial.begin(115200);
+};
 
-  Serial.println("Initialize HMC5883L");
-  while (!compass.begin())
-  {
-    Serial.println("compass not working *(yet)!");
-    delay(500);
-  }
-
-    compass.setRange(HMC5883L_RANGE_1_3GA);
- 
-  compass.setMeasurementMode(HMC5883L_CONTINOUS);
- 
-  compass.setDataRate(HMC5883L_DATARATE_15HZ);
- 
-  compass.setSamples(HMC5883L_SAMPLES_1);
+void Display::init() const
+{
+    pinMode(red_led_location_, OUTPUT);
+    pinMode(green_led_location_, OUTPUT);
 }
 
-void loop(){
-  Vector raw = compass.readRaw();
-  Vector norm = compass.readNormalize();
-  Serial.print(" Xraw = ");
-  Serial.print(raw.XAxis);
-  Serial.print(" Yraw = ");
-  Serial.print(raw.YAxis);
-  Serial.print(" Zraw = ");
-  Serial.print(raw.ZAxis);
-  Serial.print(" Xnorm = ");
-  Serial.print(norm.XAxis);
-  Serial.print(" Ynorm = ");
-  Serial.print(norm.YAxis);
-  Serial.print(" ZNorm = ");
-  Serial.print(norm.ZAxis);
-  Serial.println();  
- 
-  delay(100);
+void Display::displayValidLocation() const
+{
+    locationLedRedOff();
+    locationLedGreenOn();
+}
+
+void Display::displayInvalidLocation() const
+{
+    locationLedGreenOff();
+    locationLedRedOn();
+}
+
+void Display::locationLedGreenOff() const
+{
+    digitalWrite(green_led_location_, LOW);
+}
+
+void Display::locationLedGreenOn() const
+{
+    digitalWrite(green_led_location_, HIGH);
+}
+
+void Display::locationLedRedOff() const
+{
+    digitalWrite(red_led_location_, LOW);
+}
+
+void Display::locationLedRedOn() const
+{
+    digitalWrite(red_led_location_, HIGH);
 }
---
+++ src/main.cpp
@@ -0,0 +1,120 @@
+// Copyright © 2024 Robert Takacs
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+// files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+#include "display.h"
+
+static const int RXPin = 12, TXPin = 11;
+static const uint32_t GPSBaud = 9600;
+const int buttonPin = 2;  // the number of the pushbutton pin
+int buttonState = 0;  // variable for reading the pushbutton status
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+Display display;
+
+void displayInfo();
+
+void setup()
+{
+    Serial.begin(9600);
+    ss.begin(GPSBaud);
+
+    // initialize the pushbutton pin as an input:
+    pinMode(buttonPin, INPUT);
+    display.init();
+}
+
+void loop()
+{
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) 
+  {
+    // Todo save location
+  }
+
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+  {
+    if (gps.encode(ss.read()))
+      displayInfo();
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+   // Serial.println(F("No GPS detected: check wiring."));
+  }
+
+  delay(100);
+}
+
+void displayInfo()
+{
+    Serial.print(F("Location: ")); 
+    if (gps.location.isValid())
+    {
+        //Serial.print(gps.location.lat(), 6);
+        //Serial.print(F(","));
+        //Serial.print(gps.location.lng(), 6);
+        display.displayValidLocation();
+    }
+  else
+  {
+      //Serial.print(F("Invalid location"));
+    display.displayInvalidLocation();
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("Invalid date"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
